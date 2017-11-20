6b701054f462d37b9e1810a8fa1b1f7543d79a79
Tonio <aurelien.antonio@gmail.com>
2017-11-20 12:02:11 -0500

    Initial Commit

---
+++ ExerciceRobotsSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExerciceRobotsSwift
+//
+//  Created by Aurelien  on 20/11/2017.
+//  Copyright © 2017 Aurelien ANTONIO. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
